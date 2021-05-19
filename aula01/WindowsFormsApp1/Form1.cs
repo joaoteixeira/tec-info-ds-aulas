1783ab290d878613db2121163744b215875b3c5c
Joao Junior <joaoteixeira@ifro.edu.br>
2021-05-19 15:07:57 -0400

    código  - aula 01

---
+++ aula01/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string texto = textBoxNome.Text;
+
+            MessageBox.Show("O nome digitado foi: " + texto);
+        }
+    }
+}
