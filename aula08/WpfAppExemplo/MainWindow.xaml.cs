d1f8d2fb1137826b2bee44e823e5ea38ea24c33d
Joao Junior <joaoteixeira@ifro.edu.br>
2021-05-24 13:05:17 -0400

    Código inicial - Aula 08 - Radiobox e Checkbox

---
+++ aula08/WpfAppExemplo/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppExemplo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            string nome, data_nasc, cpf, email, telefone;
+
+            nome = txtNome.Text;
+            data_nasc = txtDataNasc.Text;
+            cpf = txtCPF.Text;
+            email = txtEmail.Text;
+            telefone = txtTelefone.Text;
+
+
+            MessageBox.Show($"Nome: {nome}\n" +
+                $"Data de Nascimento: {data_nasc}\n" +
+                $"CPF: {cpf}\n" +
+                $"E-mail: {email}\n" +
+                $"Whatsapp: {telefone}", "Informações");
+
+            ClearTextBox();
+        }
+
+        private void ClearTextBox()
+        {
+            txtNome.Text = "";
+            txtDataNasc.Text = "";
+            txtCPF.Text = "";
+            txtEmail.Text = "";
+            txtTelefone.Text = "";
+        }
+
+        private void mnuSair_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Deseja realmente sair da aplicação?", "App - Cadastro de pessoas", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+                this.Close();
+        }
+
+        private void mnuVersao_Click(object sender, RoutedEventArgs e)
+        {
+            VersionDialog vsDialog = new VersionDialog();
+            vsDialog.ShowDialog();
+        }
+
+    }
+}
