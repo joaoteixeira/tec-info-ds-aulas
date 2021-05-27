8d26a1664e2a648f4394943cf11885a93beddd7f
Joao Junior <joaoteixeira@ifro.edu.br>
2021-05-27 10:27:22 -0400

    Código inicial - Aula 10 - ComboBox

---
+++ aula10/WpfAppExemplo/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfAppExemplo
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+
+        public string Sexo { get; set; }
+
+        public bool TemFilhos { get; set; }
+    }
+}
